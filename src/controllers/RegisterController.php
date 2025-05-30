ec279e8e04167795cbe68329a04479bc774d60b2
a-simonyan <armine.simonyan100@gmail.com>
2025-05-30 17:07:00 +0400

    imp

--- src/controllers/RegisterController.php
+++ src/controllers/RegisterController.php
@@ -5,6 +5,7 @@ namespace Shop\controllers;
 use Exception;
 use Kernel\Controller\BaseController;
 use Kernel\Validator\Validator;
+use Shop\rules\RegisterRule;
 
 class RegisterController extends BaseController
 {
@@ -13,28 +14,16 @@ class RegisterController extends BaseController
      */
     public function index(): void
     {
-        $this->view()->load('Register.Index',['a' => "test"]);
+        $this->view()->load('Register.Index');
     }
 
     public function register(): void
     {
-        $rules = [
-            'username' => 'required',
-            'email' => 'required|email',
-            'password' => 'required|min:6|max:30',
-        ];
-        $messages = [
-            'username.required' => 'Username is required',
-            'email.required' => 'Email is required',
-            'email.email' => 'Email must be a valid email address',
-            'password.required' => 'Password is required',
-            'password.min' => 'Password must be at least 20 characters long',
-            'password.max' => 'Password must not exceed 30 characters',
-        ];
         $validator = Validator::make(
             $this->request()->all(),
-             $rules,$messages
-            );
+            RegisterRule::rules(),
+            RegisterRule::messages()
+        );
 
         if (!$validator->validate()) {
             $this->session()->set('errors', $validator->errors());
---
+++ src/rules/RegisterRule.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Shop\rules;
+
+use Shop\rules\interface\RulesInterface;
+
+class RegisterRule implements interface\RulesInterface
+{
+
+    public static function rules(): array
+    {
+        return  [
+            'username' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|min:6|max:30',
+        ];
+    }
+
+    public static function messages(): array
+    {
+        return  [
+            'username.required' => 'Username is required',
+            'email.required' => 'Email is required',
+            'email.email' => 'Email must be a valid email address',
+            'password.required' => 'Password is required',
+            'password.min' => 'Password must be at least 20 characters long',
+            'password.max' => 'Password must not exceed 30 characters',
+        ];
+    }
+}
